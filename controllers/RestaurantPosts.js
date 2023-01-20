0a04dc3331702f89de32b96055a85a8bfedc1346
Damin <daminhaidar08@gmail.com>
2023-01-20 11:39:26 +0100

    update

--- controllers/RestaurantPosts.js
+++ controllers/RestaurantPosts.js
@@ -1,7 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 
-import RestaurantPlat from "../Models/RestaurantPlat.js"
+import RestaurantPlat from "../models/RestaurantPlat.js"
 
 const router = express.Router()
 
