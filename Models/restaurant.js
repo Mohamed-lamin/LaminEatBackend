3db4918f1f28dda9cabc095ed170665e0fd66879
Damin <daminhaidar08@gmail.com>
2023-01-27 08:39:51 +0100

    updating

--- Models/restaurant.js
+++ Models/restaurant.js
@@ -23,41 +23,300 @@ const restaurantSchema = mongoose.Schema({
   category: categorySchema,
   dishes: [dishSchema],
 })
-
-const Restaurants = mongoose.model("Restaurant", restaurantSchema)
+const threetypeSchema = mongoose.Schema({
+  type_name: String,
+  description: String,
+  restaurant: [restaurantSchema],
+})
+const ThreetypeSchema = mongoose.model("Restaurant", threetypeSchema)
+export default ThreetypeSchema
 
 export const rmp = async () => {
-  const res = await Restaurants.create({
-    resaurant_name: "Mcdonald",
-    description: "Amuse toi avec nos repas délicieux",
-    image: "https://startling-blini-3bec23.netlify.app/images/mcdonald.jpg",
-    long: 51,
-    lat: 52,
-    address: "Route de Toulouse",
-    rating: 5,
-    category: {
-      category_name: "Offre",
-      category_image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+  const res = await ThreetypeSchema.create(
+    {
+      type_name: "Offre pres de toi",
+      description: "Pourquoi vous n'essayez pas vos restaurants locaux",
+      restaurant: [
+        {
+          resaurant_name: "Mcdonald",
+          description: "Amuse toi avec nos repas délicieux",
+          image:
+            "https://startling-blini-3bec23.netlify.app/images/mcdonald.jpg",
+          long: 51,
+          lat: 52,
+          address: "Route de Toulouse",
+          rating: 5,
+          category: {
+            category_name: "Offre",
+            category_image:
+              "https://startling-blini-3bec23.netlify.app/images/2.png",
+          },
+          dishes: [
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+          ],
+        },
+        {
+          resaurant_name: "Mcdonald",
+          description: "Amuse toi avec nos repas délicieux",
+          image:
+            "https://startling-blini-3bec23.netlify.app/images/mcdonald.jpg",
+          long: 51,
+          lat: 52,
+          address: "Route de Toulouse",
+          rating: 5,
+          category: {
+            category_name: "Offre",
+            category_image:
+              "https://startling-blini-3bec23.netlify.app/images/2.png",
+          },
+          dishes: [
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+          ],
+        },
+        {
+          resaurant_name: "Mcdonald",
+          description: "Amuse toi avec nos repas délicieux",
+          image:
+            "https://startling-blini-3bec23.netlify.app/images/mcdonald.jpg",
+          long: 51,
+          lat: 52,
+          address: "Route de Toulouse",
+          rating: 5,
+          category: {
+            category_name: "Offre",
+            category_image:
+              "https://startling-blini-3bec23.netlify.app/images/2.png",
+          },
+          dishes: [
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+          ],
+        },
+      ],
     },
-    dishes: [
-      {
-        dishname: "Cocakola",
-        description: "tres bonne",
-        price: 12,
-        image: "https://startling-blini-3bec23.netlify.app/images/2.png",
-      },
-      {
-        dishname: "Cocakola",
-        description: "tres bonne",
-        price: 12,
-        image: "https://startling-blini-3bec23.netlify.app/images/2.png",
-      },
-    ],
-  })
+    {
+      type_name: "Partenaire",
+      description: "nos partenaire",
+      restaurant: [
+        {
+          resaurant_name: "Mcdonald",
+          description: "Amuse toi avec nos repas délicieux",
+          image:
+            "https://startling-blini-3bec23.netlify.app/images/mcdonald.jpg",
+          long: 51,
+          lat: 52,
+          address: "Route de Toulouse",
+          rating: 5,
+          category: {
+            category_name: "Offre",
+            category_image:
+              "https://startling-blini-3bec23.netlify.app/images/2.png",
+          },
+          dishes: [
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+          ],
+        },
+        {
+          resaurant_name: "Mcdonald",
+          description: "Amuse toi avec nos repas délicieux",
+          image:
+            "https://startling-blini-3bec23.netlify.app/images/mcdonald.jpg",
+          long: 51,
+          lat: 52,
+          address: "Route de Toulouse",
+          rating: 5,
+          category: {
+            category_name: "Offre",
+            category_image:
+              "https://startling-blini-3bec23.netlify.app/images/2.png",
+          },
+          dishes: [
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+          ],
+        },
+        {
+          resaurant_name: "Mcdonald",
+          description: "Amuse toi avec nos repas délicieux",
+          image:
+            "https://startling-blini-3bec23.netlify.app/images/mcdonald.jpg",
+          long: 51,
+          lat: 52,
+          address: "Route de Toulouse",
+          rating: 5,
+          category: {
+            category_name: "Offre",
+            category_image:
+              "https://startling-blini-3bec23.netlify.app/images/2.png",
+          },
+          dishes: [
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type_name: "reduction",
+      description: "nos partenaire",
+      restaurant: [
+        {
+          resaurant_name: "Mcdonald",
+          description: "Amuse toi avec nos repas délicieux",
+          image:
+            "https://startling-blini-3bec23.netlify.app/images/mcdonald.jpg",
+          long: 51,
+          lat: 52,
+          address: "Route de Toulouse",
+          rating: 5,
+          category: {
+            category_name: "Offre",
+            category_image:
+              "https://startling-blini-3bec23.netlify.app/images/2.png",
+          },
+          dishes: [
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+          ],
+        },
+        {
+          resaurant_name: "Mcdonald",
+          description: "Amuse toi avec nos repas délicieux",
+          image:
+            "https://startling-blini-3bec23.netlify.app/images/mcdonald.jpg",
+          long: 51,
+          lat: 52,
+          address: "Route de Toulouse",
+          rating: 5,
+          category: {
+            category_name: "Offre",
+            category_image:
+              "https://startling-blini-3bec23.netlify.app/images/2.png",
+          },
+          dishes: [
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+          ],
+        },
+        {
+          resaurant_name: "Mcdonald",
+          description: "Amuse toi avec nos repas délicieux",
+          image:
+            "https://startling-blini-3bec23.netlify.app/images/mcdonald.jpg",
+          long: 51,
+          lat: 52,
+          address: "Route de Toulouse",
+          rating: 5,
+          category: {
+            category_name: "Offre",
+            category_image:
+              "https://startling-blini-3bec23.netlify.app/images/2.png",
+          },
+          dishes: [
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+            {
+              dishname: "Cocakola",
+              description: "tres bonne",
+              price: 12,
+              image: "https://startling-blini-3bec23.netlify.app/images/2.png",
+            },
+          ],
+        },
+      ],
+    }
+  )
   await res.save()
 }
 
 export const affich = async (req, res) => {
-  const all = await Restaurants.find()
+  const all = await ThreetypeSchema.find()
   res.send(all)
 }
