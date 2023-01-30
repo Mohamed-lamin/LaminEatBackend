b2f79b519181c2aee0dec5ca868bb1ac6f65d71e
Mohamed-Lamin TALEB <mohamed-lamin.taleb@talan.com>
2023-01-30 12:05:05 +0100

    mdp bd

--- app.js
+++ app.js
@@ -14,14 +14,18 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
 app.use("/plats", platRoutes)
+app.use('/j', (req,res)=>{
+  res.send('hello')
+})
 app.use("/user", userRoutes)
 
 const PORT = process.env.PORT || 5000
 const CONNECTIONURL = process.env.CONNECTION
 
 mongoose
-  .connect(CONNECTIONURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect("mongodb+srv://daminsoft:RFolSa9Op0jRabl5@cluster0.1mouxyv.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
+  
     app.listen(PORT, () => console.log(`Server Running on Port:${PORT}`))
   )
   .catch(error => console.log(`${error} did not connect`))
