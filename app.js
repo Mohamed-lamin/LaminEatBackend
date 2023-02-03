7d06bdfe367c5c51fd1883efa6c00f47f3b44eac
Damin <daminhaidar08@gmail.com>
2023-02-03 08:48:40 +0100

    changes

--- app.js
+++ app.js
@@ -12,24 +12,18 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
-<<<<<<< HEAD
 app.use("/", route)
-=======
-app.use("/plats", platRoutes)
-app.use('/j', (req,res)=>{
-  res.send('hello')
-})
-app.use("/user", userRoutes)
->>>>>>> b2f79b519181c2aee0dec5ca868bb1ac6f65d71e
 
 const PORT = process.env.PORT || 5000
 const CONNECTIONURL = process.env.CONNECTION
 console.log(CONNECTIONURL)
 
 mongoose
-  .connect("mongodb+srv://daminsoft:RFolSa9Op0jRabl5@cluster0.1mouxyv.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(
+    "mongodb+srv://daminsoft:RFolSa9Op0jRabl5@cluster0.1mouxyv.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
   .then(() =>
-  
     app.listen(PORT, () => console.log(`Server Running on Port:${PORT}`))
   )
   .catch(error => console.log(`${error} did not connect`))
